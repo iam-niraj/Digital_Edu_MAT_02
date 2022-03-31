768a4589f0e0aed46306865beca2012b1f86b6b6
Niraj Mestry <nirajmestry2003@gmail.com>
2022-03-31 04:26:05 -0700

    Improved Dark Mode- 'Niraj'

--- lib/utils/drawer.dart
+++ lib/utils/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:task_2/utils/Themes/dark_theme_provider.dart';
+import 'package:day_night_switcher/day_night_switcher.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
@@ -35,12 +36,25 @@ class MyDrawer extends StatelessWidget {
             trailing: Icon(Icons.send),
           ),
           ListTile(
-            leading: Text("Dark Mode"),
+            title: Text("Dark Mode"),
+            leading: DayNightSwitcherIcon(
+                  isDarkModeEnabled: themeChange.darkTheme,
+                  onStateChanged: (bool value) {
+                  themeChange.darkTheme = value;
+                }
+                ),
             trailing: Switch(
                 value: themeChange.darkTheme,
                 onChanged: (bool value) {
                   themeChange.darkTheme = value;
-                }),
+                }),    
+             
+             /* Switch(
+                value: themeChange.darkTheme,
+                onChanged: (bool value) {
+                  themeChange.darkTheme = value;
+                }), */
+
           ),
         ],
       ),
