023d24360904593360cdad7a2d90488c1bf60712
shruti2503 <shrutisahu2503@gmail.com>
2022-04-07 15:21:01 +0530

    Added validator by Shruti

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -60,7 +60,8 @@ class _SignUpPageState extends State<SignUpPage> {
                   "Sign up".text.xl5.bold.color(Colors.deepPurple).make(),
                   "Create an account".text.xl2.make(),
                   20.heightBox,
-                  CupertinoFormSection(
+                  CupertinoFormSection.insetGrouped(
+                      margin: EdgeInsets.all(0),
                       header: "Personal Details"
                           .text
                           .color(Colors.deepPurple)
@@ -70,33 +71,40 @@ class _SignUpPageState extends State<SignUpPage> {
                       children: [
                         CupertinoFormRow(
                           child: CupertinoTextFormFieldRow(
+                            textInputAction: TextInputAction.next,
                             placeholder: "Enter first name",
+                            validator: validateName,
                           ),
                           prefix: "Name".text.make(),
                         ),
                         CupertinoFormRow(
                           child: CupertinoTextFormFieldRow(
+                            textInputAction: TextInputAction.next,
                             placeholder: "Enter surname",
+                            validator: validateSurname,
                           ),
                           prefix: "Surname".text.make(),
                         ),
                         CupertinoFormRow(
                           child: CupertinoTextFormFieldRow(
-                            obscureText: true,
-                            placeholder: "Enter Phone Number",
-                          ),
-                          prefix: "Phone No.".text.make(),
-                        ),
-                        CupertinoFormRow(
-                          child: CupertinoTextFormFieldRow(
-                            obscureText: true,
-                            placeholder: "Enter Email ID",
+                            textInputAction: TextInputAction.next,
+                            placeholder: "Enter email ID",
+                            validator: validateEmail,
                           ),
                           prefix: "Email ID".text.make(),
                         ),
+                        CupertinoFormRow(
+                          child: CupertinoTextFormFieldRow(
+                            textInputAction: TextInputAction.next,
+                            placeholder: "Enter phone number",
+                            validator: validatePhone,
+                          ),
+                          prefix: "Phone No.".text.make(),
+                        ),
                       ]),
                   20.heightBox,
-                  CupertinoFormSection(
+                  CupertinoFormSection.insetGrouped(
+                      margin: EdgeInsets.all(0),
                       header: "Academic Details"
                           .text
                           .color(Colors.deepPurple)
@@ -106,7 +114,9 @@ class _SignUpPageState extends State<SignUpPage> {
                       children: [
                         CupertinoFormRow(
                           child: CupertinoTextFormFieldRow(
-                            placeholder: "Enter Roll Number",
+                            textInputAction: TextInputAction.next,
+                            placeholder: "Enter roll number",
+                            validator: validateRollno,
                           ),
                           prefix: "Roll No.".text.make(),
                         ),
@@ -165,12 +175,6 @@ class _SignUpPageState extends State<SignUpPage> {
                                     borderRadius:
                                         BorderRadius.all(Radius.circular(10.0)),
                                   ),
-                                  // dropdownBD: BoxDecoration(
-                                  //   border: Border.all(
-                                  //       color: Colors.deepPurple, width: 3),
-                                  //   borderRadius:
-                                  //       BorderRadius.all(Radius.circular(10.0)),
-                                  // ),
                                   resultBD: BoxDecoration(
                                     border:
                                         Border.all(color: Colors.deepPurple),
@@ -225,4 +229,65 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
+}
+
+String? validateName(String? value) {
+  String? patttern = r'(^[a-zA-Z ]*$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value!.length == 0) {
+    return "Username cannot be empty";
+  } else if (!regExp.hasMatch(value)) {
+    return "Username must be a-z and A-Z";
+  }
+  return null;
+}
+
+String? validateSurname(String? value) {
+  String? patttern = r'(^[a-zA-Z ]*$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value!.length == 0) {
+    return "User surname cannot be empty";
+  } else if (!regExp.hasMatch(value)) {
+    return "User surname must be a-z and A-Z";
+  }
+  return null;
+}
+
+String? validatePhone(String? value) {
+  String? patttern = r'(^[0-9]*$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value!.length == 0) {
+    return "Phone no. cannot be empty";
+  } else if (value.length != 10) {
+    return "Phone no. must be of 10 digits";
+  } else if (!regExp.hasMatch(value)) {
+    return "Phone no. must be of digits";
+  }
+  return null;
+}
+
+String? validateEmail(String? value) {
+  String pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regExp = new RegExp(pattern);
+  if (value!.length == 0) {
+    return "Email ID cannot be empty";
+  } else if (!regExp.hasMatch(value)) {
+    return "Email ID is not valid";
+  } else {
+    return null;
+  }
+}
+
+String? validateRollno(String? value) {
+  String? patttern = r'(^[0-9]*$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value!.length == 0) {
+    return "Roll no. cannot be empty";
+  } else if (value.length != 2) {
+    return "Roll no. must be of 2 digits";
+  } else if (!regExp.hasMatch(value)) {
+    return "Roll no. must be of digits";
+  }
+  return null;
 }
